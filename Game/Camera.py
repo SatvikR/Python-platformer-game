001d6e95ed75966f1c8fee759b4e6a284a50da78
SatvikR <49799352+SatvikR@users.noreply.github.com>
2020-06-22 14:47:41 -0700

    fixed camera, might merge later

--- Game/Camera.py
+++ Game/Camera.py
@@ -6,35 +6,39 @@ from .Meatball import Meatball
 from .Bullet import Bullet
 
 class Camera:
+	offset = 500
+
 	def draw_and_scroll(self, player, screen):
 		"""
 		Calcultes difference between current and initial positon
 		and draws everything accordingly
 		"""
-		offset = player.start_y - player.y
+		self.offset += (player.rect.y - self.offset - (screen.get_height() / 2 + player.rect.height / 2)) / 15
+		reversed_offset = -1 * self.offset
+		#self.offset = player.start_y - player.y
 		player.rect = player.img.get_rect(topleft=(player.x, player.y))
 		if player.direction == 'l':
-			screen.blit(pygame.transform.flip(player.img, True, False), (player.x, player.y + offset))
+			screen.blit(pygame.transform.flip(player.img, True, False), (player.x, player.y + reversed_offset))
 			screen.blit(
 				pygame.transform.flip(player.gun_img, True, False), 
-				(player.x - player.gun_img.get_width(), player.y + player.img.get_height() / 2 + offset)
+				(player.x - player.gun_img.get_width(), player.y + player.img.get_height() / 2 + reversed_offset)
 			)
 		else:
-			screen.blit(player.img, (player.x, player.y + offset))
-			screen.blit(player.gun_img, (player.x + player.img.get_width(), player.y + player.img.get_height() / 2 + offset))
+			screen.blit(player.img, (player.x, player.y + reversed_offset))
+			screen.blit(player.gun_img, (player.x + player.img.get_width(), player.y + player.img.get_height() / 2 + reversed_offset))
 
-		[plat.draw(screen, offset) for plat in Platform.platforms]
+		[plat.draw(screen, reversed_offset) for plat in Platform.platforms]
 
-		[coin.draw(screen, offset) for coin in Coin.coins]
+		[coin.draw(screen, reversed_offset) for coin in Coin.coins]
 		
 		for enemy in Enemy.enemies:
 			enemy.update()
-			enemy.draw(screen, offset)
+			enemy.draw(screen, reversed_offset)
 
 		for meatball in Meatball.meatballs:
 			meatball.update()
-			meatball.draw(screen, offset)
+			meatball.draw(screen, reversed_offset)
 
 		for bullet in Bullet.bullets:
 			bullet.update(screen)
-			bullet.draw(screen, offset)
+			bullet.draw(screen, reversed_offset)
